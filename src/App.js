116822f667d2e2c2ae5b03e319111f1bd30de7ff
codessi <johansuh@gmail.com>
2022-12-07 11:56:11 -0800

    works in ipad mini with confetti

--- src/App.js
+++ src/App.js
@@ -1,34 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
-import girl from './asset/images.jpeg'
-import gift from './asset/gift.png'
-import { useState } from 'react';
+import logo from "./logo.svg"
+import "./App.css"
+import girl from "./asset/images.jpeg"
+import gift from "./asset/gift.png"
+import confetti from './asset/confetti-25.webp'
+import { useEffect, useState } from "react"
 
 function App() {
-const [postion, setPostion] = useState(0)
+  const [position, setPosition] = useState(0)
   // tailwind
 
+  console.log(position)
+    useEffect(() => {
+    setPosition(+localStorage.getItem("position"))
 
+  
+  }, [])
+ 
+
+  useEffect(() => {
+    
+
+  }, [position])
+
+
+  
+  
+  
+  console.log(localStorage)
+
+  const handleInc = async() => {
+  
+    await position < 80 ? setPosition(position + 10) : setPosition(0)
+    localStorage.setItem('position', position);
+    
+  }
+
+  const handleDec =() => position<0? setPosition(position - 10): setPosition(0)
   return (
     <div className="App h-screen flex flex-col justify-center items-center">
-          <div>
-            <h1 className="text-1xl font-bold underline">
-                  Hello world!
-                </h1>
-                <div className='relative w-100 h-52'>
-                  <img className='absolute left-0' src={girl}></img>
-                  <img className='absolute right-0 self-center' src={gift} alt="" />
-                </div>
+      <div>
+        <h1 className="text-4xl text-red-700 mb-12 text-center font-bold "> For My Christmas Gift</h1>
+        <div className="relative w-screen border h-52 ">
+     
+          <img src={gift} className="absolute right-0" alt="" />
+          <img 
+            className={`absolute left-[${position}%] self-center`}
+            src={girl}
+          />
+         {position===80 && <img src={confetti} className="absolute left-[70%] top-[-40%]" alt="" />}
+        </div>
 
-                <div className='border mt-10 border-black border-b-4 w-[80vw]'></div>
-          </div>
-          <div className='flex'>
-          <button className='border rounded bg-red-600 text-white w-24     text-6xl '> &#8592;</button>
-            <button className='border rounded bg-red-600 text-white w-24    text-6xl ' onClick={}> &#8594;</button>
-          </div>
-
+        <div className="border mt-10 border-black border-b-4 w-screen"></div>
+      </div>
+      <div className="flex gap-24">
+        <button
+          className="border w-52 rounded bg-red-600 text-white    text-6xl "
+          onClick={handleDec}
+        >
+          {" "}
+          <div>&#8592;</div> <div>NAUTY</div>
+        </button>
+        <button
+          className="border w-52 rounded bg-red-600 text-white    text-6xl "
+          onClick={handleInc}
+        >
+          {" "}
+          &#8594; <div>NICE</div>
+        </button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
